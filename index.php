dbf9184739181fe554c161d1ffa6f6404a6f8e43
Shalva <shalvasoft@users.noreply.github.com>
2015-10-12 11:34:39 +0400

    created initialization files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * FILE: index.php
+ *
+ * @AUTHOR: LTD shalvasoft
+ * @AUTHOR: Shalva Kvaratskhelia
+ * PROJECT: MVC
+ * VERSION: 1.0.0
+ */
+
+    require_once dirname(__FILE__).'/system/Init.php';
+
---
+++ system/Init.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * FILE: Init.php
+ *
+ * @AUTHOR: LTD shalvasoft
+ * @AUTHOR: Shalva Kvaratskhelia
+ * PROJECT: MVC
+ * VERSION: 1.0.0
+ */
+     
